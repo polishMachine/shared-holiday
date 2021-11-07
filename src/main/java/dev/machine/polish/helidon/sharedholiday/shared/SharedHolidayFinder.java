378b52f1940aefc4e610d38b440c07fca4444144
polishMachine <piszczek.mateusz.dev@gmail.com>
2021-11-07 17:07:55 +0100

    determining first shared holiday from two collections

--- src/main/java/dev/machine/polish/helidon/sharedholiday/shared/SharedHolidayFinder.java
+++ src/main/java/dev/machine/polish/helidon/sharedholiday/shared/SharedHolidayFinder.java
@@ -2,10 +2,15 @@ package dev.machine.polish.helidon.sharedholiday.shared;
 
 import java.time.LocalDate;
 import java.time.Year;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.tuple.Pair;
 
 import dev.machine.polish.helidon.sharedholiday.holidays.HolidayEntry;
@@ -84,7 +89,28 @@ public class SharedHolidayFinder {
     }
 
     SharedHolidayFinder determineSharedHoliday() {
-        // TODO
+        if (CollectionUtils.isNotEmpty(holidaysIn1) && CollectionUtils.isNotEmpty(holidaysIn2)) {
+            Collection<LocalDate> intersection = CollectionUtils.intersection(
+                    getHolidaysDates(holidaysIn1),
+                    getHolidaysDates(holidaysIn2));
+            if (CollectionUtils.isNotEmpty(intersection)) {
+                LocalDate minIntersection = Collections.min(intersection);
+                sharedHoliday = Pair.of(
+                        getByDate(holidaysIn1, minIntersection).get(),
+                        getByDate(holidaysIn2, minIntersection).get());
+            }
+        }
         return this;
+    }
+
+    private List<LocalDate> getHolidaysDates(List<HolidayEntry> holidays) {
+        return holidays == null ? List.of()
+                : holidays.stream().map(h -> h.getDate())
+                        .filter(h -> dateLowLimit == null ? true : h.isAfter(dateLowLimit))
+                        .collect(Collectors.toList());
+    }
+
+    private Optional<HolidayEntry> getByDate(List<HolidayEntry> holidays, LocalDate date) {
+        return holidays.stream().filter(h -> h.getDate().isEqual(date)).findFirst();
     }
 }
