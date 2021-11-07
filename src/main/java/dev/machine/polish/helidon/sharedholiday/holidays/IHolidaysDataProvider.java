50292f92d73c2003b5887895d53a1046f66eeb69
polishMachine <piszczek.mateusz.dev@gmail.com>
2021-11-07 16:58:36 +0100

    holidays provider with mock result

--- src/main/java/dev/machine/polish/helidon/sharedholiday/holidays/IHolidaysDataProvider.java
+++ src/main/java/dev/machine/polish/helidon/sharedholiday/holidays/IHolidaysDataProvider.java
@@ -1,4 +1,4 @@
-package dev.machine.polish.helidon.sharedholiday.provider;
+package dev.machine.polish.helidon.sharedholiday.holidays;
 
 import java.util.List;
 import java.util.concurrent.CompletionStage;
@@ -6,5 +6,5 @@ import java.util.concurrent.CompletionStage;
 public interface IHolidaysDataProvider {
     
     CompletionStage<List<HolidayEntry>> getHolidays(int year, String countryCode);
-    
+
 }
